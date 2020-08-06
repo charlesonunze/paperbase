807ce6e8f94afb956a89f1571538c16d32d4c101
charlesonunze <charlesonunze2@gmail.com>
2020-08-06 23:13:19 +0100

    chore: setup paperbase theme for material ui

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Layout from "./containers/Layout";
+
+function App() {
+  return <Layout />;
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
